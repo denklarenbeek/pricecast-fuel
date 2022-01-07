af9b6fd1873c00d463b6ffe141f68d3fd5bccd0f
Dennis Klarenbeek <dennis.klarenbeek@icloud.com>
2022-01-07 14:16:44 +0100

    changed proxy

--- controller/AxiosController.js
+++ controller/AxiosController.js
@@ -2,26 +2,30 @@ const axios = require('axios');
 const url = require('url');
 
 const proxy = url.parse(process.env.QUOTAGUARDSTATIC_URL);
-const target  = url.parse('https://bbapi.pricecastfuel.com/api/analysis');
+const target  = url.parse("https://bbapi.pricecastfuel.com/api/analysis");
 
 axios.defaults.baseURL = 'https://bbapi.pricecastfuel.com/api/analysis';
 axios.defaults.headers.common['APP-key'] = process.env.API_KEY;
 
 if(process.env.NODE_ENV === 'production'){
-    axios.defaults.proxy = process.env.QUOTAGUARDSTATIC_URL,
-    axios.defaults.url = 'https://api.github.com/repos/joyent/node'
+    console.log('Application is in production and is using a proxy 💀 🐸 ')
+    axios.defaults.hostname = proxy.hostname;
+    axios.defaults.port = proxy.port || 80;
+    axios.defaults.path = target.href;
     axios.defaults.headers = {
-            'User-Agent': 'node.js'
-    }
+        "Proxy-Authorization": "Basic " + (new Buffer(proxy.auth).toString("base64")),
+        "Host" : target.hostname,
+        "APP-key": process.env.API_KEY
+      }
 }
 
 exports.getRequest = async (url) => {
     try {
         console.log(`${url} get request is fired`)
         const data = await axios.get(url);
-        console.log(`and returned a ${typeof(data)}`)
+        console.log(data, `and returned a ${typeof(data)}`)
         return data;
     } catch(e) {
         throw e
     }
-}
+}  
