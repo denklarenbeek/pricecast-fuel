8d125c952f82393f0fc0ad3abb48fe22898fd792
Dennis Klarenbeek <dennis.klarenbeek@icloud.com>
2022-01-07 14:45:18 +0100

    proxy

--- controller/AxiosController.js
+++ controller/AxiosController.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
 const url = require('url');
+const HttpsProxyAgent = require('https-proxy-agent');
+var ip = require("ip");
 
 const proxy = url.parse(process.env.QUOTAGUARDSTATIC_URL);
 const target  = url.parse("https://bbapi.pricecastfuel.com/api/analysis");
@@ -7,24 +9,40 @@ const target  = url.parse("https://bbapi.pricecastfuel.com/api/analysis");
 axios.defaults.baseURL = 'https://bbapi.pricecastfuel.com/api/analysis';
 axios.defaults.headers.common['APP-key'] = process.env.API_KEY;
 
+let options = {
+    baseURL: 'https://bbapi.pricecastfuel.com/api/analysis',
+    headers: {
+        'APP-key': process.env.API_KEY
+    }
+}
+
 if(process.env.NODE_ENV === 'production'){
     console.log('Application is in production and is using a proxy 💀 🐸 ')
-    axios.defaults.hostname = proxy.hostname;
-    axios.defaults.port = proxy.port || 80;
-    axios.defaults.path = target.href;
-    axios.defaults.headers = {
-        "Proxy-Authorization": "Basic " + (new Buffer(proxy.auth).toString("base64")),
-        "Host" : target.hostname,
-        "APP-key": process.env.API_KEY
-      }
+    // axios.defaults.hostname = proxy.hostname;
+    // axios.defaults.port = proxy.port || 80;
+    // axios.defaults.path = target.href;
+    // axios.defaults.headers = {
+    //     "Proxy-Authorization": "Basic " + (new Buffer(proxy.auth).toString("base64")),
+    //     "Host" : target.hostname,
+    //     "APP-key": process.env.API_KEY,
+    //   }
+    const endpoint = 'https://bbapi.pricecastfuel.com/api/analysis';
+    const proxy = process.env.QUOTAGUARDSTATIC_URL;
+    const agent = new HttpsProxyAgent(proxy);
+    options = {
+        uri: url.parse(endpoint),
+        agent
+    };
 }
 
 exports.getRequest = async (url) => {
     try {
+        console.dir('ip address', ip.address() )
         console.log(`${url} get request is fired`)
-        const data = await axios.get(url);
-        console.log(data, `and returned a ${typeof(data)}`)
-        return data;
+        const res = await axios.get(url, options);
+        console.log(res.request.socket.remoteAddress, `and returned a ${typeof(res)}`);
+        console.log('headers',res.request.headers)
+        return res
     } catch(e) {
         throw e
     }
