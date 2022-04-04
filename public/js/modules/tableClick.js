e686017dd0309bb6917b3b16b07685f80fbf9800
Dennis Klarenbeek <dennis.klarenbeek@icloud.com>
2022-04-04 22:14:52 +0200

    added the delete function on a document

--- public/js/modules/tableClick.js
+++ public/js/modules/tableClick.js
@@ -40,36 +40,6 @@ export function showdropdown (table) {
     }
 };
 
-function createPopup (id) {
-
-    const popupContainer = document.createElement('div');
-    popupContainer.classList.add('popup-container');
-    popupContainer.classList.add('td-action');
-    popupContainer.setAttribute('data-clickable', 'false')
-
-    const button = document.createElement('button');
-    button.textContent = 'Delete';
-    button.setAttribute('class', 'btn btn-danger');
-    button.setAttribute('data-clickable', 'false')
-    button.setAttribute('data-rowid', `${id}`)
-    button.addEventListener('click', async (e) => {
-        try {
-            console.log(id);
-            const response = await axios.delete(`${window.location.protocol}//${window.location.host}/api/report/${id}`);
-            console.log(response.data.deleted);
-            if(response.data.deleted){
-                const row = document.getElementById(id);
-                row.parentNode.removeChild(row);
-            }
-        } catch (error) {   
-            console.log(error);
-        }
-    });
-
-    popupContainer.appendChild(button)
-
-    return popupContainer;
-}
 
 export function deletePopUp (button) {
     if(!button) return
@@ -77,12 +47,19 @@ export function deletePopUp (button) {
     const buttons = document.querySelectorAll('.delete-row');
 
     buttons.map(button => {
-        button.addEventListener('click', (e) => {
-            const id = e.target.dataset.rowid
+        button.addEventListener('click', async (e) => {
+            const id = e.target.dataset.reportid
             // create a pop-up, with make sure button
             console.log(id);
-            const popup = createPopup(id);
-            button.appendChild(popup);
+            try {
+                const response = await axios.delete(`${window.location.protocol}//${window.location.host}/api/report/${id}`);
+                console.log(response);
+                const rowElement = document.getElementById(id);
+                rowElement.remove();
+                
+            } catch (error) {
+                console.log(error)
+            }
 
         })
     })
