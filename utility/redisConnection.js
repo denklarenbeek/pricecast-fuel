b4de4250e9f6d3afe0e5c90913fd20c484e7337f
Dennis Klarenbeek <dennis.klarenbeek@icloud.com>
2022-04-04 14:20:38 +0200

    changed redis option

--- utility/redisConnection.js
+++ utility/redisConnection.js
@@ -1,20 +1,12 @@
 const IORedis = require('ioredis');
-const util = require('util');
 
 const options = {
-    maxRetriesPerRequest: null,
+    maxRetriesPerRequest: 5,
     enableReadyCheck: false
 };
 
 const url = process.env.REDIS_URL || "redis://127.0.0.1:6379"
 
 const connection = new IORedis(url, options);
-
-// connection.monitor(function (err, monitor) {
-//     // Entering monitoring mode.
-//     monitor.on("monitor", function (time, args, source, database) {
-//       console.log(time + ": " + util.inspect(args));
-//     });
-//   });
 
 module.exports = connection;
